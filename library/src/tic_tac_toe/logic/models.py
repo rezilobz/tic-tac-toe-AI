2ea55bd776dfd0c56eb26446ebf793ae0c5361f3
rezilobz <rezi.lobzhanidze@gmail.com>
2023-02-12 04:12:36 +0400

    Added MiniMax

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
-import re
 import enum
+import random
+import re
 from dataclasses import dataclass
 from functools import cached_property
 
-from tic_tac_toe.logic.exceptions import InvalidMove
+from tic_tac_toe.logic.exceptions import InvalidMove, UnknownGameScore
 from tic_tac_toe.logic.validators import validate_grid, validate_game_state
 
 class Mark(str, enum.Enum):
@@ -113,6 +114,13 @@ class GameState:
             for match in re.finditer(r"\s", self.grid.cells):
                 moves.append(self.make_move_to(match.start()))
         return moves
+    
+    
+    def make_random_move(self) -> Move | None:
+        try:
+            return random.choice(self.possible_moves)
+        except IndexError:
+            return None
 
 
     def make_move_to(self, index: int) -> Move:
@@ -131,3 +139,13 @@ class GameState:
                 self.starting_mark,
             ),
         )
+        
+    def evaluate_score(self, mark: Mark) -> int:
+        if self.game_over:
+            if self.tie:
+                return 0
+            if self.winner is mark:
+                return 1
+            else:
+                return -1
+        raise UnknownGameScore("Game is not over yet")
